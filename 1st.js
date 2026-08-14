760e37aca419f9c0017fd57d310413495f4fb152
snehalsingh-01 <snehalsingh.work@gmail.com>
2026-08-14 16:02:12 +0530

    operators done

--- 1st.js
+++ 1st.js
@@ -1,4 +1,6 @@
 console.log('hello sweety');
+let z = "hello";
+console.log(z+123);
 
 //making an object (student)
 const student  = {
@@ -20,3 +22,39 @@ const Product = {
 };
 console.log(Product);
 
+// Arithmetic Operators
+
+let a = 5;
+let b = 2;
+
+console.log("a = ", a, "& b = ", b);
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+console.log("a % b = ", a % b);
+console.log("a ** b = ", a ** b); // 5^2
+
+//Comparision operator
+console.log("5 != 2", a != b);
+console.log("a == b ",a == b);
+console.log("a !== b",a !== b);
+
+//logical operator
+// Logical Operators
+
+let a1 = 6;
+let b1 = 5;
+
+let cond1 = a1 < b1; // false
+let cond2 = a1 === 6; // true
+
+console.log("cond1 && cond2 = ", a1 < b1 && a1 === 6);
+console. log ("! (6<5) =",! (a1 < b1)); //false
+
+// Terenary Operators
+
+let age = 25;
+
+let result1 = age >= 18 ? "adult" : "not adult";
+console.log(result1);
