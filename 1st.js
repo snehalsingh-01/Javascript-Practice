53b5b4bb1f4d056c41b5c541410a380ad6d71c72
snehalsingh-01 <snehalsingh.work@gmail.com>
2026-08-14 07:17:17 +0530

    learning the basics

---
+++ 1st.js
@@ -0,0 +1,22 @@
+console.log('hello sweety');
+
+//making an object (student)
+const student  = {
+    name : "Sweety",
+    theClass : 12,
+    age : 19,
+    // key : value (pair)
+};
+
+student ["age"] = student ["age"] + 1;
+console.log(student.age);
+
+const Product = {
+    name : "Parker Jotter Standard CT Ball Pen(Black)",
+    rating : 4,
+    isDeal : true,
+    price : 270,
+    offer : 5,
+};
+console.log(Product);
+
