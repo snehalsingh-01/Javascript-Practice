ca07d70bc27394a3698a63970146a0640bcc98db
snehalsingh-01 <snehalsingh.work@gmail.com>
2026-08-16 08:34:08 +0530

    .

---
+++ Array/Methods_Functions/3rd.js
@@ -0,0 +1,18 @@
+function myF(){//func define
+    console.log("Hello everyone");
+    console.log("I am practicing Javascript");
+}
+myF();//func call
+
+function Func(msg){//parameter
+    console.log(msg);
+}
+Func("Hiiiiiiiiiiiiiiii:)");//arguement
+
+//Function for calculating the sum of 2 no.s 
+function sum(x,y){// x,y are local variables
+    s = x + y;
+    return s;
+}
+let val = sum(3,7);
+console.log(val);
