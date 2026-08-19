d9ab2afd11ab1f0a4b9890e5b694f7869ec2b44c
snehalsingh-01 <snehalsingh.work@gmail.com>
2026-08-19 21:40:17 +0530

    DOM Basics

--- Array/Methods_Functions/DOM/4th.js
+++ Array/Methods_Functions/DOM/4th.js
@@ -1,1 +1,7 @@
-console.log("hello");
+console.log(document.body);
+console.log(document.head);
+console.log(window.document);
+console.log(document.body.childNodes[1]);
+
+document.body.childNodes[2].innerText = "abcd";
+console.log(document.body.childNodes[2]);
