7fc72200644297240cc6f0b8dc68e09e4f74a5c8
snehalsingh-01 <snehalsingh.work@gmail.com>
2026-08-19 22:17:15 +0530

    Query Selector

--- Array/Methods_Functions/DOM/4th.js
+++ Array/Methods_Functions/DOM/4th.js
@@ -6,7 +6,7 @@ console.log(document.body.childNodes[1]);
 document.body.childNodes[2].innerText = "abcd";
 console.log(document.body.childNodes[2]);
 
-//DOM Manipulation
+//DOM Manipulation (accessing elements of html pg)
 
 //selecting with id
 let heading = document.getElementById("Heading");
@@ -17,4 +17,26 @@ let classs = document.getElementsByClassName("context");
 console.dir(classs);
 console.log(classs);
 
+//Query Selector
+
+//selecting with tag
+let para = document.getElementsByTagName("p");
+console.dir(para);
+console.log(para);
+
+let elements = document.querySelector("p"); //access 1st element
+console.dir(elements);
+
+let allelements = document.querySelectorAll("p"); //access all elements
+console.dir(allelements);
+
+let a = document.querySelector(".context"); //access 1st element
+console.dir(a);
+
+let b = document.querySelectorAll(".context"); //access all elements
+console.dir(b);
+
+//In tag, it cannot access all elements
+let c = document.querySelector("#Heading"); //access 1st element
+console.dir(c);
 
