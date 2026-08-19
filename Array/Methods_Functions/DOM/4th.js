b39dd22cc050ec7fb01b67585ae9192b2d34a1bb
snehalsingh-01 <snehalsingh.work@gmail.com>
2026-08-19 22:03:28 +0530

    DOM Manipulation

--- Array/Methods_Functions/DOM/4th.js
+++ Array/Methods_Functions/DOM/4th.js
@@ -5,3 +5,16 @@ console.log(document.body.childNodes[1]);
 
 document.body.childNodes[2].innerText = "abcd";
 console.log(document.body.childNodes[2]);
+
+//DOM Manipulation
+
+//selecting with id
+let heading = document.getElementById("Heading");
+console.dir(heading);
+
+//selecting with class
+let classs = document.getElementsByClassName("context");
+console.dir(classs);
+console.log(classs);
+
+
