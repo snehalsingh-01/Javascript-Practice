cc51ff92368d6a3fd2a00f116395be7426274310
snehalsingh-01 <snehalsingh.work@gmail.com>
2026-08-20 08:45:30 +0530

    Attributes

---
+++ Array/Methods_Functions/DOM/DOM-2/5th.js
@@ -0,0 +1,16 @@
+//Attributes
+
+let div = document.querySelector("div");
+console.log(div);
+
+let id = div.getAttribute("id");
+console.log(id);
+
+let myclass = document.querySelector(".myclass");
+console.log(myclass);
+
+let id2 = myclass.getAttribute("class");
+console.log(id2);
+
+console.log(myclass.setAttribute("class","newClass"));
+
