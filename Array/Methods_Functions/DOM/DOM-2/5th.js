52aefe074a16346f8308057499c6f6ea6d4fb54e
snehalsingh-01 <snehalsingh.work@gmail.com>
2026-08-20 09:48:36 +0530

    practiceQ

--- Array/Methods_Functions/DOM/DOM-2/5th.js
+++ Array/Methods_Functions/DOM/DOM-2/5th.js
@@ -1,6 +1,6 @@
 //Attributes
 
-let div = document.querySelector("div");
+/*let div = document.querySelector("div");
 console.log(div);
 
 let id = div.getAttribute("id");
@@ -36,3 +36,21 @@ newHeading.innerText = ("Javascript");;
 console.log(newHeading);
 
 document.querySelector("body").prepend(newHeading);
+
+//to remove elements
+newHeading.remove();
+*/
+
+
+//Q1. Create a new button element. Give it a text “click me”, background color of red & text color of white. Insert the button as the first element inside the <body> tag.
+let newBtn = document.createElement("button");
+newBtn.style.background = ("red");
+newBtn.style.color = ("white");
+newBtn.innerHTML = ("Click me!") ;
+console.log(newBtn);
+document.querySelector("body").prepend(newBtn);
+
+//Q2. Create a <p> tag in HTML, give it a class & some styling. Now create a new class in CSS and try to append this class to the <p> element. Did you notice, how you overwrite the class name when you add a new class? Solve this problem using classList.
+let para = document.querySelector("p");
+para.classList.add("newClass");
+para.classList.remove("newClass");
