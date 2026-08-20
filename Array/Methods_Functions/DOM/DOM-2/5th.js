b98657a6231370938f09425e0c588c2e3feb1645
snehalsingh-01 <snehalsingh.work@gmail.com>
2026-08-20 09:17:27 +0530

    .

--- Array/Methods_Functions/DOM/DOM-2/5th.js
+++ Array/Methods_Functions/DOM/DOM-2/5th.js
@@ -14,3 +14,25 @@ console.log(id2);
 
 console.log(myclass.setAttribute("class","newClass"));
 
+//style
+div.style.fontSize = "25";
+console.dir(div.style);
+//div.innerText = "THIS IS A DIV";
+
+//inserting elements
+
+let newBtn = document.createElement("button");
+newBtn.innerText = "Click me!";
+console.log(newBtn);
+
+let div1 = document.querySelector("div");
+//div1.append(newBtn);
+//div1.prepend(newBtn);
+//div1.before(newBtn);
+div1.after(newBtn);
+
+let newHeading = document.createElement("h1");
+newHeading.innerText = ("Javascript");;
+console.log(newHeading);
+
+document.querySelector("body").prepend(newHeading);
