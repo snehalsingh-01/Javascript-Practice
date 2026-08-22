efb52567fa574ac53062f6440215e5e93c888f40
snehalsingh-01 <snehalsingh.work@gmail.com>
2026-08-22 23:59:39 +0530

    Event Listener

--- EVENTS/6th.js
+++ EVENTS/6th.js
@@ -1,21 +1,48 @@
 let btn1 = document.querySelector('#btn1');
 
-btn1.onclick = () => {
+/*btn1.onclick = () => {
     onclick="console.log('The button was clicked.')";
     alert('helloooooooo');
     let a = 25;
     a++;
     console.log(a);
+};*/
+
+//Event Listener
+btn1.addEventListener("click", () => {
+    console.log('The button was clicked.');
+    let a = 25;
+    a++;
+    console.log(a);
+});
+
+const chai = (e) => {
+    console.log(e);
 };
+
+btn1.addEventListener("click", chai);
+
+btn1.addEventListener("contextmenu", () => {
+    console.log('Right Clicked!');
+});
+
+//Removing Event Listener
+
+btn1.removeEventListener("click", chai);
 
 let btn2 = document.querySelector('#btn2');
 
-btn2.ondblclick = (evt) => {
+/*btn2.ondblclick = (evt) => {
     console.log('This button was clicked 2x.');
     console.log(evt);
     console.log("Event Type =",evt.type);
     console.log("Event Target =",evt.target);
-};
+};*/
+
+btn2.addEventListener("dblclick", () => {
+    console.log('The button was clicked 2x.');
+});
+
 
 let div = document.querySelector('div');
 div.onmouseover = () => {
